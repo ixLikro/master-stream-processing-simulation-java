0f36f48fd5de5a760ca5411f3c688ae4c50de61f
Ole Schleeßelmann <o.schleesselmann@ostfalia.de>
2019-11-27 11:44:33 +0100

    reduce the chance of bad weather in the summer

--- src/de/stream/processing/g6/simulator/WeatherSimulator.java
+++ src/de/stream/processing/g6/simulator/WeatherSimulator.java
@@ -3,7 +3,9 @@ package de.stream.processing.g6.simulator;
 import de.stream.processing.g6.util.RandomHelper;
 import de.stream.processing.g6.util.Weather;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class WeatherSimulator extends Simulator {
 
@@ -21,10 +23,30 @@ public class WeatherSimulator extends Simulator {
         return Weather.values()[RandomHelper.getInteger(0, Weather.values().length-1)];
     }
 
+    private Weather getRandomWeather(Date simTime){
+
+        //reduce the chance of bad weather in the summer
+        Calendar temp = new GregorianCalendar();
+        temp.setTimeInMillis(simTime.getTime());
+        int month = temp.get(Calendar.MONTH);
+
+        if(month == Calendar.JUNE || month == Calendar.JULY){
+            //summer, 70% chance of good weather
+            if(RandomHelper.hitPercentChance(70d)){
+                //50:50 clear sky ot slightly cloudy
+                return RandomHelper.hitPercentChance(50d) ? Weather.CLEAR_SKY : Weather.SLIGHTLY_CLOUDY;
+            }
+        }else {
+            return Weather.values()[RandomHelper.getInteger(Weather.CLOUDY.ordinal(), Weather.values().length-1)];
+        }
+        return Weather.values()[RandomHelper.getInteger(0, Weather.values().length-1)];
+    }
+
     @Override
     public void simulate(Date simTime) {
         currentWeather = comingWeather;
-        comingWeather = getRandomWeather();
+        comingWeather = getRandomWeather(simTime);
+        //the weather changes between 20 min and 3h
         simulationInterval = RandomHelper.getInteger(20 * 60, 180 *60);
     }
 
